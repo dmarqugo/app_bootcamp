f1e58934db328fed6e967720716b5c0edab71206
USERSAD\dmarqugo <dmarqugo@emeal.nttdata.com>
2022-11-18 16:09:35 +0000

    Projeto Bootcamp

---
+++ app-escola-bootcamp/escola/src/main/java/com/nttdata/escola/model/Aluno.java
@@ -0,0 +1,41 @@
+package com.nttdata.escola.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "aluno")
+public class Aluno {
+
+
+    @Id
+    private Long nif;
+    private String nome;
+    private Integer idade;
+    private Integer escolaridade;
+
+    public Aluno(Long nif, String nome, Integer idade, Integer escolaridade) {
+        this.nif = nif;
+        this.nome = nome;
+        this.idade = idade;
+        this.escolaridade = escolaridade;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" +
+                "nif=" + nif +
+                ", nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", escolaridade='" + escolaridade + '\'' +
+                '}';
+    }
+}
